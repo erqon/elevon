6f590ea47137eb45db4d03841e7389c887ca4d78
Giorgi Merebashvili <mereba2627@gmail.com>
2026-06-27 21:19:01 +0400

    Update cargo workspace

--- apps/dashboard/main.ts
+++ apps/dashboard/main.ts
@@ -1,4 +1,4 @@
 import { App, staticFiles } from "fresh";
-import { type State } from "./utils.ts";
+import type { State } from "./utils.ts";
 
 export const app = new App<State>().use(staticFiles()).fsRoutes();
