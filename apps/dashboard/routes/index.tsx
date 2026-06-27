002172ab9cf914e10e5ae59602afce39f7ea92c5
Giorgi Merebashvili <mereba2627@gmail.com>
2026-06-27 18:07:25 +0400

    Initial commit

---
+++ apps/dashboard/routes/index.tsx
@@ -0,0 +1,11 @@
+import { define } from "@/utils.ts";
+
+export default define.page(() => {
+  return (
+    <div class="px-4 py-8 mx-auto min-h-screen">
+      <div class="max-w-3xl mx-auto flex flex-col items-center justify-center">
+        Dashboard
+      </div>
+    </div>
+  );
+});
