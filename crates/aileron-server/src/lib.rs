b696cd813dd0ea184c0e2c7321ca149cca59ae15
Giorgi Merebashvili <mereba2627@gmail.com>
2026-07-04 22:54:24 +0400

    Start working on auth

--- crates/aileron-server/src/lib.rs
+++ crates/aileron-server/src/lib.rs
@@ -1,7 +1,9 @@
 mod app;
 pub mod cli;
 pub mod db;
+mod dto;
 mod routes;
+mod services;
 
 pub async fn run(config: aileron_config::Config) -> anyhow::Result<()> {
     app::serve(config).await
---
+++ crates/aileron-server/src/services/auth.rs
@@ -0,0 +1,27 @@
+use anyhow::Result;
+use argon2::{
+    Argon2,
+    password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString, rand_core::OsRng},
+};
+
+pub fn hash_password(password: String) -> Result<String> {
+    let salt = SaltString::generate(&mut OsRng);
+    let argon2 = Argon2::default();
+    let password_hash = argon2
+        .hash_password(password.as_bytes(), &salt)
+        .map_err(|err| anyhow::anyhow!("Failed to hash password: {err}"))?
+        .to_string();
+
+    Ok(password_hash)
+}
+
+pub fn verify_password(password: String, password_hash: String) -> Result<bool> {
+    let argon2 = Argon2::default();
+    let parsed_hash = PasswordHash::new(&password_hash)
+        .map_err(|err| anyhow::anyhow!("Failed to verify password: {err}"))?;
+
+    Ok(argon2
+        .verify_password(password.as_bytes(), &parsed_hash)
+        .is_ok())
+}
+
