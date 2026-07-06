bc26a589dc692220d05260a7c1bf8efb84e52bdd
Giorgi Merebashvili <mereba2627@gmail.com>
2026-07-06 20:45:03 +0400

    Update create_user

--- crates/aileron-server/src/cli.rs
+++ crates/aileron-server/src/cli.rs
@@ -1,22 +1,12 @@
 use aileron_config::Config;
-use argon2::{
-    Argon2,
-    password_hash::{PasswordHasher, SaltString, rand_core::OsRng},
-};
 
 use crate::db::{get_db, models::user::User};
+use crate::services::auth::hash_password;
 
 pub async fn create_user(config: Config, email: String, password: String) -> anyhow::Result<()> {
-    let salt = SaltString::generate(&mut OsRng);
-    let argon2 = Argon2::default();
-
-    let password_hash = argon2
-        .hash_password(password.as_bytes(), &salt)
-        .map_err(|err| anyhow::anyhow!("Failed to hash password: {err}"))?
-        .to_string();
-
     let mut db = get_db(&config.database).await?;
 
+    let password_hash = hash_password(&password)?;
     toasty::create!(User {
         email,
         password: password_hash
