1fd118ec87279cfd561e09b122e9415d6287100f
Giorgi Merebashvili <mereba2627@gmail.com>
2026-07-19 12:22:41 +0400

    cli: Add styles and about description

--- crates/aileron-cli/src/main.rs
+++ crates/aileron-cli/src/main.rs
@@ -1,10 +1,30 @@
-use clap::Parser;
+use clap::{
+    Parser,
+    builder::styling::{AnsiColor, Styles},
+};
+
+const STYLES: Styles = Styles::styled()
+    .header(AnsiColor::Yellow.on_default())
+    .usage(AnsiColor::Green.on_default())
+    .literal(AnsiColor::Green.on_default())
+    .placeholder(AnsiColor::Green.on_default());
 
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
+#[command(
+    name = "aileron",
+    version,
+    about,
+    long_about = "Aileron: A zero-SSH, agent-driven container orchestrator and Pingora-powered edge proxy featuring a unified dashboard to monitor, manage, and load-balance server clusters with zero downtime.",
+    styles = STYLES
+)]
 struct Args {
-    #[arg(short, long, default_value = "aileron.yml", help = "Path to the config file")]
-    config: Option<String>
+    #[arg(
+        short,
+        long,
+        default_value = "aileron.yml",
+        help = "Path to the config file"
+    )]
+    config: Option<String>,
 }
 
 #[tokio::main]
