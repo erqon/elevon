2a600d5e607b4fc1ce2fec8261fef8ebd778c3c6
Giorgi Merebashvili <mereba2627@gmail.com>
2026-07-20 14:12:25 +0400

    proxy: Add test load balancer example

---
+++ crates/aileron-proxy/src/bin/test-server.rs
@@ -0,0 +1,29 @@
+use axum::{Router, routing::get};
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(long, short, default_value_t = 3000)]
+    port: usize,
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let cli = Cli::parse();
+
+    let app = Router::new().route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind(format!("127.0.0.1:{}", cli.port))
+        .await
+        .unwrap();
+    tracing::info!("listening on http://{}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    tracing::info!("Endpoint got hit!");
+
+    "Hello, World!"
+}
--- crates/aileron-proxy/src/main.rs
+++ crates/aileron-proxy/src/main.rs
@@ -1,3 +1,50 @@
+use async_trait::async_trait;
+use pingora::prelude::*;
+use std::sync::Arc;
+
+pub struct LB(Arc<LoadBalancer<RoundRobin>>);
+
+#[async_trait]
+impl ProxyHttp for LB {
+    /// For this small example, we don't need context storage
+    type CTX = ();
+    fn new_ctx(&self) -> () {
+        ()
+    }
+
+    async fn upstream_peer(&self, _session: &mut Session, _ctx: &mut ()) -> Result<Box<HttpPeer>> {
+        let upstream = self
+            .0
+            .select(b"", 256) // hash doesn't matter for round robin
+            .unwrap();
+
+        println!("upstream peer is: {upstream:?}");
+
+        let peer = Box::new(HttpPeer::new(upstream, false, String::new()));
+        Ok(peer)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut my_server = Server::new(None).unwrap();
+    my_server.bootstrap();
+
+    let mut upstreams =
+        LoadBalancer::try_from_iter(["127.0.0.1:3001", "127.0.0.1:3002", "127.0.0.1:3003"])
+            .unwrap();
+
+    let hc = TcpHealthCheck::new();
+    upstreams.set_health_check(hc);
+    upstreams.health_check_frequency = Some(std::time::Duration::from_secs(1));
+
+    let background = background_service("health check", upstreams);
+    let upstreams = background.task();
+
+    let mut lb = http_proxy_service(&my_server.configuration, LB(upstreams));
+    lb.add_tcp("0.0.0.0:6188");
+
+    my_server.add_service(background);
+    my_server.add_service(lb);
+
+    my_server.run_forever();
 }
