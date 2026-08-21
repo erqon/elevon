d75e0dab474095b6584e9700b79dd3c24fed7d0b
Giorgi Merebashvili <mereba2627@gmail.com>
2026-08-21 21:40:41 +0400

    fixes

---
+++ crates/elevon-deploy/src/cli/init.rs
@@ -0,0 +1,23 @@
+use std::{fs, path::PathBuf};
+
+pub fn run() -> anyhow::Result<()> {
+    tracing::info!("Initializing config file ...");
+
+    let config_dir = PathBuf::from(".elevon");
+
+    if config_dir.exists() {
+        tracing::warn!(".elevon directory already exists, exiting.");
+        return Ok(());
+    }
+
+    fs::create_dir(&config_dir)?;
+
+    let config_path = config_dir.join("deploy.yml");
+    let config_content = include_str!("../../templates/config.yml");
+
+    fs::write(config_path, config_content)?;
+
+    tracing::info!("Config file was created at .elevon/deploy.yml");
+
+    Ok(())
+}
